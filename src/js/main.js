729179650eec974238cd0085965af3e9c708fb6e
Harman Pannu <iharmanpannu@gmail.com>
2018-03-17 23:12:02 -0700

    almost done

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,66 @@
-import { directive as onClickaway } from 'vue-clickaway';
+// Vue.directive('click-outside', {
+//   bind() {
+//     let self = this;
+//     this.event = function(event) {
+//       self.vm.$emit(self.expression, event);
+//     };
+//     this.el.addEventListener('click', this.stopProp);
+//     document.body.addEventListener('click', this.event);
+//   },
+
+//   unbind() {
+//     this.el.removeEventListener('click', this.stopProp);
+//     document.body.removeEventListener('click', this.event);
+//   },
+
+//   stopProp(event) {
+//     event.stopPropagation();
+//   }
+// });
+
+// new Vue({
+//   el: '#app',
+//   data: {
+//     visible: false,
+//     home: true,
+//     show: false,
+//     active: false,
+//     subNavActive: false,
+//     safNavShow: false,
+//     repNavUl: false,
+//     admNavShow: false,
+//     rotateDropDown: false
+//   },
+//   methods: {},
+//   events: {
+//     closeEvent: function() {
+//       console.log('close event called');
+//       this.hide();
+//     }
+//   }
+// });
+'use strict';
+
+Vue.directive('click-outside', {
+  bind: function bind() {
+    var self = this;
+    this.event = function(event) {
+      self.vm.$emit(self.expression, event);
+    };
+    this.el.addEventListener('click', this.stopProp);
+    document.body.addEventListener('click', this.event);
+  },
+  unbind: function unbind() {
+    this.el.removeEventListener('click', this.stopProp);
+    document.body.removeEventListener('click', this.event);
+  },
+  stopProp: function stopProp(event) {
+    event.stopPropagation();
+  }
+});
 
 new Vue({
   el: '#app',
-  directives: {
-    onClickaway: onClickaway
-  },
   data: {
     visible: false,
     home: true,
@@ -16,10 +72,11 @@ new Vue({
     admNavShow: false,
     rotateDropDown: false
   },
-  methods: {
-    away: function() {
-      this.safNavShow = false;
-      console.log('clicked away');
+  methods: {},
+  events: {
+    closeEvent: function closeEvent() {
+      console.log('close event called');
+      // this.hide();
     }
   }
 });
